5f72d94266744b6725b00b8a23ce0d73511bb683
MauLight <maulisseluz@gmail.com>
2022-11-11 21:15:31 -0300

    first commit

---
+++ src/components/songlist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <ul className="list-group">
+                <li className="list-group-item">Valley of the Damned</li>
+                <li className="list-group-item">Everything's Magic</li>
+                <li className="list-group-item">The Bard's Song</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
